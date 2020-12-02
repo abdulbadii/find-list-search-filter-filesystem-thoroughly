855fa15023b2e30b3426bb85f5b096b2057f1f38
abdulbadii <abdulbadii@yandex.com>
2020-12-02 22:23:26 +0700

    remove

--- list-from-bashrc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-d=$(cd `dirname ${0}`&&pwd)
-cp -u ~/.bashrc $d/../bashrc
-
-n=${1:+-$1}
-sed -Ene "/^fxr\(\)\{\s+#{5,}\s*BEGINNING OF l,/,/^\}\s+#{5,}\s*ENDING OF l,/{s/^li\(\)/l\(\)/;tn;/^\}\s+#{5,}\s*ENDING OF/{wlist-su$n.sh" -e "q}; :n wlist-su$n.sh" -e '}' ~/.bashrc
-sed -En "s/\bsudo\s+//; wlist$n.sh" list-su$n.sh
