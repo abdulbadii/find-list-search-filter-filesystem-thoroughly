134d6aa17ce3d1db45957919fc7b423c8d8de5b3
abdulbadii <abdulbadii@yandex.com>
2021-03-29 17:36:28 +0000

    fix

--- InstallUpdate2.bashrc.sh
+++ InstallUpdate2.bashrc.sh
@@ -23,31 +23,35 @@ if((n));then
 		read -N1 -t5 o;	[ "$o" = u ]&&T=list.sh
 		if [ "$1" = -n?* ]	;then	m=${1:2}
 			echo -ne "\nYou commanded to update, install it as function name $m... "
-			sed -e "$n e sed 's/^l\\(\\)\\{/$m(){/' $T" -e "$n,${nn}d" ~/.bashrc &&echo done now, invoke it by $m
+			sed -e "$n e sed 's/^l\\(\\)\\{/$m(){/' $T" -e "$n,${nn}d" ~/.bashrc &&echo done, now should be invoked by $m
 		else
 			sed -i -e "${n}r$T" -e "$n,${nn}d" ~/.bashrc;fi
 	fi
 else
+	echo -e "\nNot found a line with pattern\n\t\"$s\"\nas the header mark"
 	for e;{
 	case "$e" in
 	-n) m=${e:2}
 		echo -e "\nYou commanded to install it as function name $m... "
-		sed -e "$n e sed 's/^l\\(\\)\\{/$m(){/' $T" -e "$n,${nn}d" ~/.bashrc &&echo done now, invoke it by $m;;
+		sed -e "$n e sed 's/^l\\(\\)\\{/$m(){/' $T" -e "$n,${nn}d" ~/.bashrc &&echo done, now should be invoked by $m;;
 	-[1-9]*)	n=${e:1}
 		if [ $n = 1 ] ;then
-			sed -i -e "1{r$T" -e 'h;d};2{x;G}' ~/.bashrc
+			echo -ne "\nYou commanded to install it at line number $n... "
+			sed -i -e "1{r$T" -e 'h;d};2{x;G}' ~/.bashrc &&echo done
 		else
-			sed -i -e "$((n-1))r$T" ~/.bashrc;fi
-		break;;
+			echo -ne "\nInstalling at first position of ~/.bashrc... "
+			sed -i -e "$((n-1))r$T" ~/.bashrc &&echo done
+		fi;break;;
 	-p?*)
 		p=${e:2}
-		sed -i -ne "/$p/{r$T" -e 'bo};b;:o n;bo'  ~/.bashrc;;
-	-u)T=list.sh;break;;
+		sed -i -ne "/$p/{r$T" -e 'bo};b;:o n;bo'  ~/.bashrc
+		break;;
+	-u)T=list.sh;;
 	*)
-	echo -e "Not found a line with pattern\n\t\"$s\"\nas the header mark\nPlease command by instruction below to install/insert $T to ~/.bashrc or just hit Enter on next\n"
+	echo -e "\nPlease command by instruction below to install/insert $T to ~/.bashrc or just hit Enter on next\n"
 	echo -e "\nUsage to install or copy/insert \"$T\" into \"$HOME/.bashrc\" is to position such:\n"
 	echo -e "\n - At line number:       $N -{1 ..999}\n - At line with pattern:   $N -p{pattern string}"
-	echo -e "\n - Set tool/function name else than 'l':  $N -n{string}\n\t e.g. $N -nfd"
+	echo -e "\n - Set the tool/function name else than default 'l':  $N -n{string}\n\t e.g. $N -nfd"
 	echo -e "\n - The sudo/root privilege is always on when using this, to install one without it, put -u as first option such\n $N -u ...\n\n"
 	read -sN1 -p "Copy $T onto the first line of ~/.bashrc (Enter: yes, else: no)? " o
 	[ "$o" = $'\x0a' ]&&{
